3c21dfef14805300657332262abe425d76604558
KvrlvP <paolaz2005d@gmail.com>
2026-06-20 14:01:43 -0500

    test: agregar prueba para MinishopApplication.main()

---
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTest.java
@@ -0,0 +1,11 @@
+package com.tecsup.minishop;
+
+import org.junit.jupiter.api.Test;
+
+class MinishopApplicationTest {
+
+    @Test
+    void mainMethodShouldRunWithoutErrors() {
+        MinishopApplication.main(new String[]{});
+    }
+}
